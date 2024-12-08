8a271cefbf98b3d2d9d271efff9a974f57bc4c14
Liu,Yu <code.liuyu@gmail.com>
2024-12-08 17:01:33 +0800

    Add LoadData Demo

---
+++ src/main/scala/com/innovationv2/SparkCore/CH3_Datasource/LoadData.scala
@@ -0,0 +1,46 @@
+package com.innovationv2.SparkCore.CH3_Datasource
+
+import com.innovationv2.AppBase
+import org.apache.spark.rdd.{JdbcRDD, RDD}
+import org.junit.Test
+
+import java.sql.{DriverManager, ResultSet}
+
+class LoadData extends AppBase("LoadDataDemo") {
+  @Test
+  def loadDataFromFS(): Unit = {
+    sc.textFile("wc.txt", 2)
+      .foreach(println)
+  }
+
+  @Test
+  def loadDataFromDB(): Unit = {
+    val conn = () => DriverManager.getConnection("jdbc:mysql://localhost:3306/test", "root", "123456")
+    val resMapping: ResultSet => (Int, String, String, Int) = (rs: ResultSet) => {
+      val id = rs.getInt(1)
+      val name = rs.getString(2)
+      val role = rs.getString(3)
+      val salary = rs.getInt(4)
+      (id, name, role, salary)
+    }
+
+    val rdd = new JdbcRDD[(Int, String, String, Int)](sc, conn,
+      "select id,name,role,salary from person where ? <= id and id <= ?",
+      1, 100,
+      2, resMapping)
+    rdd.foreach(println)
+  }
+
+  @Test
+  def loadDataFromLocal(): Unit = {
+    val lst = List(
+      (1, "Hello Spark"),
+      (2, "Hello Alice"),
+      (3, "Hello Bob"),
+      (4, "Hello Cici"),
+      (5, "Hello Doge")
+    )
+    val rdd: RDD[(Int, String)] = sc.makeRDD(lst)
+    rdd.foreach(println)
+  }
+}
